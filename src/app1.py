bee48b8c5cbfde82832092f205c9871f9308515d
hitbug0 <hitbug.0@gmail.com>
2024-06-12 22:28:52 +0900

    バックエンドからの応答が画面上に保持されるようにした

--- src/app1.py
+++ src/app1.py
@@ -1,6 +1,4 @@
-import concurrent.futures
 import threading
-import time
 
 import requests
 import streamlit as st
@@ -9,28 +7,52 @@ from streamlit.runtime.scriptrunner import add_script_run_ctx
 SERVER_URL = "http://127.0.0.1:8000"
 
 
-def button1_action(c):
-    response = requests.get(f"{SERVER_URL}/get1")
-    c.write(response.json())  # 表示できない。サブプロセスの処理だからと思われる
+def button_action(time_, words_):
+    response = requests.get(f"{SERVER_URL}/wait/{time_}/{words_}")
+    st.session_state["response" + words_[-1]] = response.json()
     print(response.json())  # こっちはコマンドプロンプトに表示される
 
 
-def button2_action(c):
-    response = requests.get(f"{SERVER_URL}/get2")
-    c.write(response.json())  # 表示できない。サブプロセスの処理だからと思われる
-    print(response.json())  # こっちはコマンドプロンプトに表示される
-
-
+thread1 = None
+thread2 = None
 if __name__ == "__main__":
-    b1 = st.empty()
-    b2 = st.empty()
-    c1 = st.empty()
-    c2 = st.empty()
+
+    if "response1" not in st.session_state:
+        st.session_state["response1"] = ""
+
+    if "response2" not in st.session_state:
+        st.session_state["response2"] = ""
+
+    col1, col2 = st.columns(2)
+    b1 = col1.empty()
+    t1 = col1.empty()
+
+    b2 = col2.empty()
+    t2 = col2.empty()
+
+    time1 = t1.slider("wait time 1", 0.02, 5.0, step=0.01)
+    time2 = t2.slider("wait time 2", 0.02, 5.0, step=0.01)
+
     if b1.button("Button 1"):
-        thread1 = threading.Thread(target=button1_action, args=(c1,))
+        thread1 = threading.Thread(target=button_action, args=(time1, "Button 1"))
         add_script_run_ctx(thread1)
         thread1.start()
+
     if b2.button("Button 2"):
-        thread2 = threading.Thread(target=button2_action, args=(c2,))
+        thread2 = threading.Thread(target=button_action, args=(time2, "Button 2"))
         add_script_run_ctx(thread2)
         thread2.start()
+
+    if thread1:
+        thread1.join()
+        print(st.session_state.response1)
+        st.rerun()
+
+    if thread2:
+        thread2.join()
+        print(st.session_state.response2)
+        st.rerun()
+
+    col1.write(st.session_state.response1)
+
+    col2.write(st.session_state.response2)
--- src/main.py
+++ src/main.py
@@ -1,37 +1,25 @@
 import asyncio
+
 from fastapi import FastAPI
 from fastapi.responses import JSONResponse
 
 app = FastAPI()
 
-@app.get("/get1")
-async def get1():
-    ret = await long_time()
+
+@app.get("/wait/{time_}/{words_}")
+async def wait_time(time_: float, words_: str):
+    ret = await sleep_time(time_, words_)
     return JSONResponse(content=ret)
 
-async def long_time():
-    print("long:  start")
-    await asyncio.sleep(3)
-    print("long:  end")
-    return {"message": "long end"}
 
-
-@app.get("/get2")
-async def get2():
-    ret = await short_time()
-    return JSONResponse(content=ret)
-
-async def short_time():
-    print("short: start")
-    await asyncio.sleep(0.01)
-    print("short: end")
-    return {"message": "short end"}
-
-
-
-
+async def sleep_time(time_, words_):
+    print(f"{words_}: sleep {time_} seconds")
+    await asyncio.sleep(time_)
+    print(f"{words_}: wake up")
+    return {"message": f"{words_} slept in {time_} seconds"}
 
 
 if __name__ == "__main__":
     import uvicorn
+
     uvicorn.run(app, host="127.0.0.1", port=8000)
